d0bf093427cd371f14f737a56c97a83dbf4f7fef
manoranjanjetty <manoranjanjetty@gmail.com>
2023-07-14 20:29:06 +0100

    New Project

--- src/TicTac.java
+++ src/TicTac.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 
-public class TicTacToe{
-    public static void main(String[] args){
-    Rungame();
+public class TicTac {
+    public static void main(String args){
+        Rungame();
     }//end main
 
     public static void Rungame(){
-    String winner ="";
-    boolean XTurn = true;
-    int theRow= 0;
-    int theCol = 0;
-    String[][] gameboard = new String[3][3];
+        String winner ="";
+        boolean XTurn = true;
+        int theRow= 0;
+        int theCol = 0;
+        String[][] gameboard = new String[3][3];
 
-    InitializeGame(gameboard);
+        InitializeGame(gameboard);
 
-    //Print initial board
+        //Print initial board
         PrintCurrentboard(gameboard);
 
         //""Signal no winner
@@ -49,55 +49,55 @@ public class TicTacToe{
     }//end RunGame
 
     public static void InitializeGame(String[][] gameboard){
-    for(int i=0; i<gameboard.length; i++){
-        for(int j=0; j<gameboard[i].length; j++){
-            gameboard[i][j] = " ";
-        }//end for j
-    }//end for i
+        for(int i=0; i<gameboard.length; i++){
+            for(int j=0; j<gameboard[i].length; j++){
+                gameboard[i][j] = " ";
+            }//end for j
+        }//end for i
     }//end Ingame
 
     public static void PrintCurrentboard(String[][] gameboard){
-    for(int i=0; i<gameboard.length; i++){
-        for(int j=0; j<gameboard[i].length; j++){
-            System.out.print(gameboard[i][j]);
-            if(j<2){
-                System.out.print(" | ");
+        for(int i=0; i<gameboard.length; i++){
+            for(int j=0; j<gameboard[i].length; j++){
+                System.out.println(gameboard[i][j]);
+                if(j<2){
+                    System.out.print(" | ");
+                }
+            }//end for J
+            System.out.println();
+            if(i<2){
+                System.out.print("- - - - -");
             }
-        }//end for J
-        System.out.println();
-        if(i<2){
-            System.out.print("- - - - -");
-        }
-        //to add some space between the board
-        System.out.println();
-    }//end for i
+            //to add some space between the board
+            System.out.println();
+        }//end for i
     }//end PCB
 
     public static void GetUserInput(boolean XTurn, String[][] gameboard){
-    Scanner keyboard = new Scanner(System.in);
-    int row = -1;
-    int col = -1;
-    boolean keepAsking = true;
+        Scanner keyboard = new Scanner(System.in);
+        int row = -1;
+        int col = -1;
+        boolean keepAsking = true;
 
-    while(keepAsking){
-        System.out.println("Please enter the row THEN " + "the column, each from 0,1 or 2, " + "seperated by a space");
+        while(keepAsking){
+            System.out.println("Please enter the row THEN " + "the column, each from 0,1 or 2, " + "seperated by a space");
 
-        row = keyboard.nextInt();
-        col = keyboard.nextInt();
-        keyboard.nextLine();//consume new line
+            row = keyboard.nextInt();
+            col = keyboard.nextInt();
+            keyboard.nextLine();//consume new line
 
-        //if we are bounds with Row and column
-        if(row >= 0 && row <= 2 && col >= 0 && col <= 2){
-            //valid and in-range selection
-            //but, is it occupied by an X or O already?
-            if(!CellAlreadyOccupied(row,col, gameboard)){
-                keepAsking = false ;
-            }
-            else{
-                System.out.println("That cell is already occupied!");
-            }//end if-else
-        }//end if
-    }//end while
+            //if we are bounds with Row and column
+            if(row >= 0 && row <= 2 && col >= 0 && col <= 2){
+                //valid and in-range selection
+                //but, is it occupied by an X or O already?
+                if(!CellAlreadyOccupied(row,col, gameboard)){
+                    keepAsking = false ;
+                }
+                else{
+                    System.out.println("That cell is already occupied!");
+                }//end if-else
+            }//end if
+        }//end while
         //by the time it get's here, we know it's a valid row and column
         //in range and not already occupied
         if(XTurn){
@@ -114,14 +114,14 @@ public class TicTacToe{
     }//end CAO
 
     public static String Getwinner(String[][] gameboard){
-    final int rows = gameboard.length;
-    final int cols = gameboard[0].length;
+        final int rows = gameboard.length;
+        final int cols = gameboard[0].length;
 
-    for(int i=0; i<rows; i++){
-        if(!gameboard[i][0].equals(" ") && gameboard[i][0].equals(gameboard[i][1]) && gameboard[i][1].equals(gameboard[i][2])) {
-            return gameboard[i][0];
-        }//end if
-    }//end for
+        for(int i=0; i<rows; i++){
+            if(!gameboard[i][0].equals(" ") && gameboard[i][0].equals(gameboard[i][1]) && gameboard[i][1].equals(gameboard[i][2])) {
+                return gameboard[i][0];
+            }//end if
+        }//end for
         //check columns
         for(int i=0; i<cols; i++) {
             if (!gameboard[0][i].equals(" ") && gameboard[0][i].equals(gameboard[1][i]) && gameboard[1][i].equals(gameboard[2][i])) {
@@ -143,15 +143,16 @@ public class TicTacToe{
     }//end getwinner
 
     public static boolean IsboardFull(String[][] gameboard){
-int countFill = 0;
+        int countFill = 0;
 
-for(int i=0; i<gameboard.length; i++){
-    for(int j=0; j< gameboard[i].length; j++){
-        if(!gameboard[i][j].equals(" ")){
-            countFill++;
-        }
-    }//end for J
-}//end for I
+        for(int i=0; i<gameboard.length; i++){
+            for(int j=0; j< gameboard[i].length; j++){
+                if(!gameboard[i][j].equals(" ")){
+                    countFill++;
+                }
+            }//end for J
+        }//end for I
         return countFill == 9;
     }//end IBF
 }//end class
+
